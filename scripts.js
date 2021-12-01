ac5361a905dd39549540f83465a502b2248f868b
Henry Gu <hlgu@usc.edu>
2021-12-01 13:39:14 -0800

    Revert "updated scripts"
    This reverts commit 6a1000cf05c6c4b78d2e42e07b59b8a8631a0a44.

--- scripts.js
+++ scripts.js
@@ -21,31 +21,5 @@ document.addEventListener('DOMContentLoaded', function () {
         });
       });
     }
-    
-    var btn = document.querySelector('#showModal');
-    console.log(btn);
-    var modalDlg = document.querySelector('#image-modal');
-    var imageModalCloseBtn = document.querySelector('#image-modal-close');
-    btn.addEventListener('click', function(){
-      console.log("clicked")
-      modalDlg.classList.add('is-active');
-    });
-
-    imageModalCloseBtn.addEventListener('click', function(){
-      modalDlg.classList.remove('is-active');
-    });
+  
   });
-
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("content-tab");
-  for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " is-active";
-}
